5ef057b127b63d78d474c5a20fdfde51f2e4d118
Ankita Gaikwad <ankitagaikwad571@gmail.com>
2025-03-23 21:45:43 +0530

    Add files via upload

---
+++ NULL AND NOT NULL VALUES.sql
@@ -0,0 +1,31 @@
+USE  store;
+SELECT * FROM customers;
+-- find out the null values
+SELECT * 
+FROM customers 
+WHERE phone IS NULL;
+
+SELECT * 
+FROM order_items
+WHERE order_id IS NULL;
+
+SELECT *
+FROM order_statuses
+WHERE order_status_id IS NULL;
+
+-- how to findout the not null values
+SELECT *
+FROM customers
+WHERE customer_id  IS NOT NULL;
+
+SELECT * 
+FROM order_items
+WHERE order_id IS  NOT NULL;
+
+SELECT *
+FROM order_statuses
+WHERE order_status_id IS NOT NULL;
+
+SELECT *
+FROM shippers
+WHERE shipper_id IS NOT NULL;
